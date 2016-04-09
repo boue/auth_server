261493e84ea7a5dbeffa6998cbd0a90a8e863fa1
Germain Boue <boue.germain@gmail.com>
2016-04-08 21:29:11 -0700

    Creation of new user works and persists in DB, also recognizes if a user already has an email and returns err

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -8,12 +8,24 @@ exports.signup = function(req, res, next) {
 
   //See if user with given email exists to avoid dupes
   User.findOne({ email: email }, function(err, existingUser){
-    
+    if (err) { return next(err); }
+
+    //If user with email does exist, return err
+    if (existingUser){
+      //.status sets http code on response
+      return res.status(422).send({ error: 'Email is in use'});
+    }
+
+    //If user with email doesn't exist, create and save user reacord
+    const user = new User({
+      email: email,
+      password: password
+    });
+    user.save(function(err) {
+      if (err) { return next(err); }
+
+    //Respond to request indicating the user was created  
+    res.json(user);
+    });
   });
-
-  //If user with email does exist, return err
-
-  //If user with email doesn't exist, create and save user reacord
-
-  //Respond to request indicating the user was created
 }
