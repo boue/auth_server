6fc080b612463be14ff9c826781d6044a17dc633
Germain Boue <boue.germain@gmail.com>
2016-04-08 21:04:42 -0700

    Pseudocode to figure out how to deal with incoming users

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,4 +1,12 @@
 //put in some logic from post req signup route
 exports.signup = function(req, res, next) { 
-  res.send({ success: 'true' } );
+  //Pull data from request obj
+
+  //See if user with given email exists to avoid dupes
+
+  //If user with email does exist, return err
+
+  //If user with email doesn't exist, create and save user reacord
+
+  //Respond to request indicating the user was created
 }
