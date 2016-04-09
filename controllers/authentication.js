2646adb71f67a4cd304b0f51387e9680817ef58d
Germain Boue <boue.germain@gmail.com>
2016-04-08 21:16:57 -0700

    Able to see if user has existing email and we pull off email and password from a req

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,8 +1,15 @@
+const User = require('../models/user');
+
 //put in some logic from post req signup route
 exports.signup = function(req, res, next) { 
   //Pull data from request obj
+  const email = req.body.email;
+  const password = req.body.password;
 
   //See if user with given email exists to avoid dupes
+  User.findOne({ email: email }, function(err, existingUser){
+    
+  });
 
   //If user with email does exist, return err
 
